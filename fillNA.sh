9ba3d7af0677bf7cda6aa0345af94b0ffdc64faf
cjieming <cjieming@gmail.com>
2016-08-15 17:50:26 -0700

    add fillNA.sh

---
+++ fillNA.sh
@@ -0,0 +1,4 @@
+## USAGE
+## Setting FS and OFS to tab ensures the output is correctly delimited. The for-loop looks at every field and set it to zero if they're empty. The one at the end is a shorthand for { print $0 }.
+
+awk 'BEGIN { FS = OFS = "\t" } { for(i=1; i<=NF; i++) if($i ~ /^ *$/) $i = "NA" }; 1' $1 
