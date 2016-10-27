acd09fb5f938c2a09a80032efa54d74060442b86
cjieming <cjieming@gmail.com>
2016-10-27 13:46:34 -0700

    vcf2sql.py debugged

--- vcf2sql.py
+++ vcf2sql.py
@@ -43,7 +43,7 @@ def load_data_from_file(data_file_name, target_table):
     createtable = "CREATE TABLE " + tablename + """ (
                    sample_ID  VARCHAR(20) NOT NULL,
                    dbSNP  VARCHAR(20),
-                   genotype CHAR(2) ) """
+                   genotype CHAR(2) ); """
     cursor.execute(createtable)
 
     ## add data
