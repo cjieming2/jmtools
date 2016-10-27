05bae095b88aca7d30c3d26832062a53b40e6eae
cjieming <cjieming@gmail.com>
2016-10-27 13:39:38 -0700

    vcf2sql.py debugged

--- vcf2sql.py
+++ vcf2sql.py
@@ -49,13 +49,8 @@ def load_data_from_file(data_file_name, target_table):
    ## add data
    sql = Template("""
                            LOAD DATA LOCAL INFILE "$file"
-                           INTO TABLE $table FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';""")
+                           INTO TABLE $table CHARACTER SET UTF8 FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';""")
    sql = sql.substitute(file=data_file_name, table=target_table)
-
-   # sql = Template("""
-   #         LOAD DATA LOCAL INFILE "$file"
-   #         INTO TABLE $table FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' (id,gsm,val) SET pk = null;""")
-   # sql = sql.substitute(file=data_file_name, table=target_table)
 
    cursor.execute(sql)
    connection.commit()
